d55869da27d5546c204a189fde74241c21d5df67
C.Dufficy <c.dufficy@gmail.com>
2023-05-16 11:24:51 +0100

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';    
+import './App.css';          
+import {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App() {            
+  // we can write JS here :) 
+
+  const [greeting, setGreeting] = useState("Hello World") //set the openign string
+  const [total, setTotal] = useState(0);
+
+
+  const handleClick = (name) => {
+    setGreeting("Hello " + name)
+  }
+  // const handleClick = () => {
+  //   setGreeting("Button was clicked")}
+
+  const deposit = () => {
+    setTotal(total + 10)
+  }
+  
+  return ( 
+    <>                 
+     <h1>{greeting}</h1>  
+      <button onClick={() => handleClick("Colette")}>Change Greeting  </button>
+
+      <p>Total: £{total}</p>
+      <button onClick={deposit}>Deposit</button>
+     </>
+  );                         
+};                         
+
 
 export default App;
